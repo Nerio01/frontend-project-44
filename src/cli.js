fa8317f33efaea56e6a32e5fdb0a1cd30f4bb29a
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-07 22:38:41 +0300

    added src directory with cli.js file inside

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+const namePrompt = readlineSync.question('May I have your name? ');
+const savedName = `Hello, ${namePrompt}!`;
+console.log(savedName);
+};

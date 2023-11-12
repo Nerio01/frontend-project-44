83ff8640a9b03829c47a31b8831e5c9a8e8985cc
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-12 11:42:18 +0300

    relocated game logic to game_logic directory and imported it into brain-even.js

---
+++ game_logic/brain-even-logic.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+
+export const brainEven = () => {
+    const namePrompt = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${namePrompt}!`);
+    let numberOfWins = 0;
+    let answer = '';
+    console.log('Answer "yes" if the number is even, otherwise answer "no"');
+    while (numberOfWins !== 3) {
+        const currentRandomNumber = _.random(1, 100);
+        if (currentRandomNumber % 2 === 0) {
+            answer = 'yes';
+        } else {
+            answer = 'no'
+        }
+        console.log(`Question ${currentRandomNumber}`);
+        const answerPrompt = readlineSync.question('You answer: ');
+        if (answerPrompt == answer) {
+            console.log('Correct!');
+            numberOfWins += 1;
+            if (numberOfWins === 3) {
+                console.log(`Congratulations, ${namePrompt}!`);
+                break
+            }
+        } else {
+            numberOfWins = 0;
+            console.log(`'${answerPrompt}' is wrong answer ;(. Correct answer was '${answer}'.`);
+            console.log(`Let's try again, ${namePrompt}!`)
+            break
+        }
+    };
+    
+};
+
+export default brainEven;

06e9891d3a81cdc0d94f0e29482765183318adcc
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-18 23:29:42 +0300

    added brain-gcd game to the project and tested it, still need to record ASCII video of brain-gcd and brain-calc

---
+++ games/brain-gcd-logic.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import { gcd } from 'mathjs';
+
+const getRandomNumber = () => {
+    const randomNumber = _.random(1,100);
+    return randomNumber;
+};
+
+export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
+
+export const brain_gcd = () => {
+    const number_1 = getRandomNumber();
+    const number_2 = getRandomNumber();
+    const answer = gcd(number_1, number_2);
+    const question_gcd = `Question: ${number_1} ${number_2}`;
+    return [question_gcd, answer];
+}
+
+export default brain_gcd;

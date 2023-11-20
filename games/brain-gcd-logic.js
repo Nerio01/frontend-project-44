25e9162b2773b252fc65dc09515c245a286c0062
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-20 23:56:12 +0300

    Back to square one. Fuck.

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -4,11 +4,11 @@ import { gcd } from 'mathjs';
 export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
 
 export const brain_gcd = () => {
-    const randomNumber1 = _.random(1,100)
-    const randomNumber2 = _.random(1,100)
-    ///const questionGcd = `${randomNumber1} ${randomNumber2}`
-    const answer = gcd(randomNumber1, randomNumber2)
-    return [`${randomNumber1} ${randomNumber2}`, answer]
+    const randomNumber1 = _.random(1,100);
+    const randomNumber2 = _.random(1,100);
+    const questionGcd = `${randomNumber1} ${randomNumber2}`;
+    const answer = gcd(randomNumber1, randomNumber2);
+    return [questionGcd, answer];
 };
  
 export default brain_gcd;

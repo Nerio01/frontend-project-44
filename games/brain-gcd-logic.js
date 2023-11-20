8683f11f46d0b0a43e57528bcc06b75a9df2a995
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-20 22:23:43 +0300

    removed extra ; at the end of line 8

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -5,7 +5,7 @@ export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
 
 export const brain_gcd = () => {
     const randomNumber1 = _.random(1,100);
-    const randomNumber2 = _.random(1,100);;
+    const randomNumber2 = _.random(1,100);
     const questionGcd = `Question: ${randomNumber1} ${randomNumber2}`;
     const answer = gcd(randomNumber1, randomNumber2);
     return [questionGcd, answer];

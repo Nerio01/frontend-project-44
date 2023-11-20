8243ecec6e3f70a5a953d2d28e20ca273456c835
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-20 20:41:36 +0300

    removed redundant function used for generating random number

--- games/brain-calc-logic.js
+++ games/brain-calc-logic.js
@@ -2,11 +2,6 @@ import _ from 'lodash';
 
 export const question_calc = 'What is the result of the expression?';
 
-const getRandomNumber = () => {
-    const randomNumber = _.random(1,100);
-    return randomNumber;
-};
-
 const getRandomOperator = () => {
     const listOfOperators = ['+', '-', '*'];
     const operator = listOfOperators[_.random(0,2)];
@@ -14,8 +9,8 @@ const getRandomOperator = () => {
 };
 
 export const getExpressionAndPrintStatement = () => {
-    const random_number1 = getRandomNumber();
-    const random_number2 = getRandomNumber();
+    const random_number1 = _.random(1,100);
+    const random_number2 = _.random(1,100);
     const random_operator = getRandomOperator();
     const expressionPrint = `Question: ${random_number1} ${random_operator} ${random_number2}`;
     switch (random_operator) {

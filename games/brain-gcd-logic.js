d6a6fec0eac5cce8fe0b8e1d166d11521fd34dae
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-11-20 14:28:27 +0000

    reverted last commit, no effect :(

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -12,9 +12,8 @@ export const brain_gcd = () => {
     const number_1 = getRandomNumber();
     const number_2 = getRandomNumber();
     const question_gcd = `Question: ${number_1} ${number_2}`;
-    const totally_legit_gcd = `${question_gcd}`;
     const answer = gcd(number_1, number_2);
-    return [totally_legit_gcd, answer];
+    return [question_gcd, answer];
 }
 
 export default brain_gcd;

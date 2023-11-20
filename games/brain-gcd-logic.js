2469e1bfd12c0b890f6206a1d0b0b512c30193f7
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-20 20:30:43 +0300

    removed redundant function for generating random numbers

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -1,16 +1,11 @@
 import _ from 'lodash';
 import { gcd } from 'mathjs';
 
-const getRandomNumber = () => {
-    const randomNumber = _.random(1,100);
-    return randomNumber;
-};
-
 export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
 
 export const brain_gcd = () => {
-    const randomNumber1 = getRandomNumber();
-    const randomNumber2 = getRandomNumber();
+    const randomNumber1 = _.random(1,100);
+    const randomNumber2 = _.random(1,100);;
     const questionGcd = `Question: ${randomNumber1} ${randomNumber2}`;
     const answer = gcd(randomNumber1, randomNumber2);
     return [questionGcd, answer];

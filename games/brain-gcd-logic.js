7f169022ccdf16ba3586ee515a3694eeb5d23da3
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-11-20 14:22:47 +0000

    made extra redundant changes to description line to see the test resuls

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -12,8 +12,9 @@ export const brain_gcd = () => {
     const number_1 = getRandomNumber();
     const number_2 = getRandomNumber();
     const question_gcd = `Question: ${number_1} ${number_2}`;
+    const totally_legit_gcd = `${question_gcd}`;
     const answer = gcd(number_1, number_2);
-    return [question_gcd, answer];
+    return [totally_legit_gcd, answer];
 }
 
 export default brain_gcd;

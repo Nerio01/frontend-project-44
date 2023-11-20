c56c143104ba77c70fd1a199c2d3b663cf833db9
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-20 23:48:54 +0300

    directly inserted fstring into return array

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -6,9 +6,9 @@ export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
 export const brain_gcd = () => {
     const randomNumber1 = _.random(1,100)
     const randomNumber2 = _.random(1,100)
-    const questionGcd = `${randomNumber1} ${randomNumber2}`
+    ///const questionGcd = `${randomNumber1} ${randomNumber2}`
     const answer = gcd(randomNumber1, randomNumber2)
-    return [String(questionGcd), answer]
+    return [`${randomNumber1} ${randomNumber2}`, answer]
 };
  
 export default brain_gcd;

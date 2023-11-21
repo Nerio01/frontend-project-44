0c22caeef3753d08b30830b34ece3dd92066503c
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-22 00:40:43 +0300

    rewrote import

--- games/brain-prime-logic.js
+++ games/brain-prime-logic.js
@@ -1,11 +1,11 @@
-import mathJs from "math.js";
 import _ from "lodash";
+import { isPrime } from "mathjs";
 
 export const start_phrase_prime = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 export const brain_prime = () => {
     const generated_number = _.random(1,100);
-    const prime_check = mathJs.isPrime(generated_number);
+    const prime_check = isPrime(generated_number);
     let answer = '';
     if (prime_check === true) {
         answer = 'yes'

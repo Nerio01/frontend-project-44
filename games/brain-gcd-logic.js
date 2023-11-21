ad5039563ba868bea68faac0f5e47c051afd2f12
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-11-21 09:04:54 +0300

    Reverted previous commit as it didn't work.

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -6,8 +6,7 @@ export const gcdQuestion = 'Find the greatest common divisor of given numbers.';
 export const brain_gcd = () => {
     const randomNumber1 = _.random(1,100);
     const randomNumber2 = _.random(1,100);
-    const questionGcdArray = [randomNumber1, randomNumber2];
-    const question_gcd = questionGcdArray.join(' '); 
+    const question_gcd = `${randomNumber1} ${randomNumber2}`; 
     const answer = gcd(randomNumber1, randomNumber2);
     return [question_gcd, answer];
 };

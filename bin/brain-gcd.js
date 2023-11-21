c89b574ee654ecedad4b08d9ec6bb414a109a734
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-11-21 09:17:47 +0000

    reordered imports and ensured that that all imports to this file from same file are done via {}

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameFlow from "../src/index.js";
+import { brain_gcd } from "../games/brain-gcd-logic.js";
 import { gcdQuestion } from "../games/brain-gcd-logic.js";
-import brain_gcd from "../games/brain-gcd-logic.js";
 
 gameFlow(brain_gcd, gcdQuestion);

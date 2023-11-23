2854a7f355d67ced02eacac19b35270a3760b84e
Nerio_Ozran <nerios.cave@gmail.com>
2023-11-24 00:01:13 +0300

    reduced range of randon numbers from 1-100 to 1-10. If THAT whas the reason, im gonna kill somebody

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -3,8 +3,8 @@ import { gcd } from 'mathjs';
 
 export default () => {
   const startPhraseGcd = 'Find the greatest common divisor of given numbers.';
-  const randomNumber1 = _.random(1, 100);
-  const randomNumber2 = _.random(1, 100);
+  const randomNumber1 = _.random(1, 10);
+  const randomNumber2 = _.random(1, 10);
   const questionGcd = `${randomNumber1} ${randomNumber2}`;
   const answer = gcd(randomNumber1, randomNumber2);
 

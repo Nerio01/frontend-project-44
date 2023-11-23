bbc41914bd94d7f31efdb05fcc293e440716e1f4
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-11-23 11:13:50 +0000

    fixed inting errors of brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import gameFlow from '../src/index.js';
-import { brain_gcd, start_phrase_gcd } from '../games/brain-gcd-logic.js';
+import { brainGcd, startPhraseGcd } from '../games/brain-gcd-logic.js';
 
-gameFlow(brain_gcd, start_phrase_gcd);
+gameFlow(brainGcd, startPhraseGcd);
--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import { gcd } from 'mathjs';
 
-export const start_phrase_gcd = 'Find the greatest common divisor of given numbers.';
+export const startPhraseGcd = 'Find the greatest common divisor of given numbers.';
 
-export const brain_gcd = () => {
+export const brainGcd = () => {
   const randomNumber1 = _.random(1, 100);
   const randomNumber2 = _.random(1, 100);
-  const question_gcd = `${randomNumber1} ${randomNumber2}`;
+  const questionGcd = `${randomNumber1} ${randomNumber2}`;
   const answer = gcd(randomNumber1, randomNumber2);
 
-  return [question_gcd, answer];
+  return [questionGcd, answer];
 };

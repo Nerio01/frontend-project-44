e07e6f787ef09593ef196a77c10e7e143e4e2a78
Nerio01 <65695074+Nerio01@users.noreply.github.com>
2023-12-13 18:33:42 +0300

    Update brain-gcd-logic.js

--- games/brain-gcd-logic.js
+++ games/brain-gcd-logic.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { gcd } from 'mathjs';
 
 export default () => {
-  const startPhraseGcd = 'Find the greatest common divisor of given numbers.';
+  const startPhraseGcd = 'Find the greatest common divisor of given numbers';
   const randomNumber1 = _.random(1, 100);
   const randomNumber2 = _.random(1, 100);
   const questionGcd = `${randomNumber1} ${randomNumber2}`;

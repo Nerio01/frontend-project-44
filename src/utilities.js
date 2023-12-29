09ac396a6048be6c9c6076d191a6f6e2670c4b7a
Nerio_Ozran <nerios.cave@gmail.com>
2023-12-29 16:58:17 +0300

    added default value to function parameter <max> to make it work with a single value input

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,4 @@
-export default (min, max) => {
+export default (min, max = 1) => {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
   return Math.floor(Math.random() * (maxValue - minValue) + minValue);

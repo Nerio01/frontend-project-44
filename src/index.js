3c5a5ec0d595fe3aa3faab0279b07eb279f3a39a
Nerio_Ozran <nerios.cave@gmail.com>
2023-12-29 15:36:48 +0300

    removed redundant variables and reworked round counter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
 import readlineSync from 'readline-sync';
 
 export default (gameLogic) => {
-  const maxRounds = 3;
-  let numberOfRounds = 0;
+  let remainingRounds = 3;
 
   console.log('Welcome to the Brain Games!');
   const userNamePrompt = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userNamePrompt}!`);
-  let startPhraseUsed = 0;
 
-  while (numberOfRounds !== maxRounds) {
+  while (remainingRounds !== 0) {
     const [startPhraseLine, questionLine, trueAnswer] = gameLogic();
-    if (startPhraseUsed === 0) {
-      console.log(startPhraseLine);
-    }
+    console.log(startPhraseLine);
     console.log(`Question: ${questionLine}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === trueAnswer) {
-      numberOfRounds += 1;
+      remainingRounds -= 1;
       console.log('Correct!');
-      startPhraseUsed = 1;
-      if (numberOfRounds === maxRounds) {
+      if (remainingRounds === 0) {
         console.log(`Congratulations, ${userNamePrompt}!`);
-        startPhraseUsed = 0;
         break;
       }
     } else {
       console.log(`'${userAnswer}' is the wrong answer ;(. Correct answer was '${trueAnswer}'.`);
       console.log(`Let's try again, ${userNamePrompt}!`);
-      startPhraseUsed = 0;
       break;
     }
   }

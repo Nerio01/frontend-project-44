7a381ada3923398643ded46b444b40fa5527bd9d
Nerio_Ozran <nerios.cave@gmail.com>
2023-12-29 16:29:35 +0300

    Reworked switch stattement with better default and moved return value to end of the function

--- games/brain-calc-logic.js
+++ games/brain-calc-logic.js
@@ -1,29 +1,34 @@
-import _ from 'lodash';
+import random from '../src/utilities.js';
 
 const getRandomOperator = () => {
   const listOfOperators = ['+', '-', '*'];
-  const operator = listOfOperators[_.random(0, 2)];
+  const operator = listOfOperators[random(0, 2)];
   return operator;
 };
 
 export default () => {
   const startPhraseCalc = 'What is the result of the expression?';
-  const randomNumber1 = _.random(1, 100);
-  const randomNumber2 = _.random(1, 100);
+  const randomNumber1 = random(1, 100);
+  const randomNumber2 = random(1, 100);
   const randomOperator = getRandomOperator();
   const expressionPrint = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+  let answer = '';
   switch (randomOperator) {
     case '+': {
-      return [startPhraseCalc, expressionPrint, String(randomNumber1 + randomNumber2)];
+      answer = String(randomNumber1 + randomNumber2);
+      break;
     }
     case '-': {
-      return [startPhraseCalc, expressionPrint, String(randomNumber1 - randomNumber2)];
+      answer = String(randomNumber1 - randomNumber2);
+      break;
     }
     case '*': {
-      return [startPhraseCalc, expressionPrint, String(randomNumber1 * randomNumber2)];
+      answer = String(randomNumber1 * randomNumber2);
+      break;
     }
     default: {
-      return 'something went very wrong here';
+      throw new Error(`Unknown operator: '${randomOperator}'!`);
     }
   }
+  return [startPhraseCalc, expressionPrint, answer];
 };

d565271c211acf8f1775578a3da21d4b8645389e
Nerio_Ozran <nerios.cave@gmail.com>
2024-01-03 12:46:48 +0300

    rewrote brain-games to import single function from cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import promptUserName from '../src/cli.js';
+import greetUserByName from '../src/cli.js';
 
-export const greetAndPromptUserName = () => {
-  console.log('Welcome to the Brain Games!');
-  promptUserName();
-};
-greetAndPromptUserName();
-export default greetAndPromptUserName;
+greetUserByName();

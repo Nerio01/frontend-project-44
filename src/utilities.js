80800cd106af18e10aa34966b1c8a4030c9defee
Nerio_Ozran <nerios.cave@gmail.com>
2024-01-03 12:53:06 +0300

    rewrote random function to be more universal

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,5 @@
-export default (min, max = 1) => {
+export default (min = 0, max = 100) => {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
-  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+  return Math.floor(minValue + Math.random() * (maxValue - minValue + 1));
 };

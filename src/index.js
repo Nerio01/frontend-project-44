46b2876a6b8113388e536a63c57697f80b92b4c6
Nerio_Ozran <nerios.cave@gmail.com>
2024-01-03 16:22:33 +0300

    reworked engine with the use of constant to store maximum no of rounds, also made it a for loop

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import readlineSync from 'readline-sync';
 
 export default (gameLogic) => {
-  let remainingRounds = 3;
+  const totalRounds = 3;
 
   console.log('Welcome to the Brain Games!');
   const userNamePrompt = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userNamePrompt}!`);
 
-  while (remainingRounds !== 0) {
+  for (let rounds = 0; rounds < totalRounds; rounds += 1) {
     const [startPhraseLine, questionLine, trueAnswer] = gameLogic();
-    console.log(startPhraseLine);
+    if (rounds === 0) {
+      console.log(startPhraseLine);
+    }
     console.log(`Question: ${questionLine}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === trueAnswer) {
-      remainingRounds -= 1;
       console.log('Correct!');
-      if (remainingRounds === 0) {
-        console.log(`Congratulations, ${userNamePrompt}!`);
-        break;
-      }
     } else {
       console.log(`'${userAnswer}' is the wrong answer ;(. Correct answer was '${trueAnswer}'.`);
       console.log(`Let's try again, ${userNamePrompt}!`);
-      break;
+      return;
     }
   }
+  console.log(`Congratulations, ${userNamePrompt}!`);
 };
